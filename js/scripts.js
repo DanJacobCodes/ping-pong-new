281467391473c2fa7ca2c932957bba4293d68440
Dan Jacob <dansamueljacob@gmail.com>
2017-04-02 16:53:32 -0400

    add input form, headers, and list of rules

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,19 @@ var counter = [];
 
 function pingPong(numbers) {
   if (numbers % 3 === 0) {
-    return 'Ping!'
+    return 'Ping!';
   } else if (numbers % 5 === 0) {
-    return 'Pong!'
-  } else if (numbers % 15 === 0)
-    return 'Ping Pong!'  
+    return 'Pong!';
+  } else if (numbers % 15 === 0){
+    return 'Ping Pong!';
+}   else {
+  return numbers;
+  }
+}
+
+function returnedNumbers(numbers) {
+   if (n) {
+
+   }
+
 }
