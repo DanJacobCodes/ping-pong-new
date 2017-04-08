1446513bf0ce6b172208af21e70e0dc46db2dc97
Dan Jacob <dansamueljacob@gmail.com>
2017-04-08 17:18:52 -0400

    add more descriptive variable and function names

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
-//Business Logic
-var counter = [];
+//Business Logic//
+var pingPongList = [];
 
 function pingPong(numbers) {
-  if (numbers % 3 === 0) {
-    return 'Ping!';
+  if (numbers % 15 === 0) {
+    return 'Ping Pong!';
   } else if (numbers % 5 === 0) {
     return 'Pong!';
-  } else if (numbers % 15 === 0){
-    return 'Ping Pong!';
-}   else {
+
+  } else if (numbers % 3 === 0){
+    return 'Ping !';
+  }
+  else {
   return numbers;
   }
 }
@@ -17,9 +19,22 @@ function returnedNumbers(numbers) {
    if (isNan(numbers)) {
      alert("Oops!Please enter a number")
    } else {
-     for (var i = 1; i < numbers.length; i++) {
+     for (var i = 1; i <=numbers; i++) {
        returnedNumbers.push(pingPong(i));
      }
    }
 
 }
+ //User Interface Logic//
+ $(document).ready(function() {
+   $("form#pingPongGame").submit(function(event){
+      event.preventDefault();
+
+      var userInput = parseInt($("form#input").val());
+      pingPongList = [];
+
+      returnedNumbers(userInput);
+
+   })
+
+ })
