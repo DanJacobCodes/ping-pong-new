d75211eb74286df482b1d809903e395194ba9fc0
Dan Jacob <dansamueljacob@gmail.com>
2017-04-08 20:27:15 -0400

    add proper indentation and spacing to js file

--- js/scripts.js
+++ js/scripts.js
@@ -7,37 +7,35 @@ function pingPong(numbers) {
   } else if (numbers % 5 === 0) {
     return "Pong";
 
-  } else if (numbers % 3 === 0){
+  } else if (numbers % 3 === 0) {
     return "Ping";
-  }
-  else {
-  return numbers;
+  } else {
+    return numbers;
   }
 }
 
 function returnedNumbers(numbers) {
-   if (isNaN(numbers) || (numbers < 0)) {
-     alert("Oops!Please enter a  valid number")
-   }
-   else {
-     for(var i = 1; i <=numbers; i++) {
-       pingPongList.push(pingPong(i));
-     }
-   }
+  if (isNaN(numbers) || (numbers < 0)) {
+    alert("Oops!Please enter a  valid number")
+  } else {
+    for (var i = 1; i <= numbers; i++) {
+      pingPongList.push(pingPong(i));
+    }
+  }
 }
- //User Interface Logic//
- $(document).ready(function() {
-   $("form#pingPongGame").submit(function(event) {
-      event.preventDefault();
+//User Interface Logic//
+$(document).ready(function() {
+  $("form#pingPongGame").submit(function(event) {
+    event.preventDefault();
 
-      var userInput = parseInt($("#input").val());
-      pingPongList = [];
+    var userInput = parseInt($("#input").val());
+    pingPongList = [];
 
-      returnedNumbers(userInput);
+    returnedNumbers(userInput);
 
-   $("#output").empty();
-      pingPongList.forEach(function(numbers) {
-        $("ul#output").append("<li>" + numbers + "</li>");
-      });
-   });
- });
+    $("#output").empty();
+    pingPongList.forEach(function(numbers) {
+      $("ul#output").append("<li>" + numbers + "</li>");
+    });
+  });
+});
